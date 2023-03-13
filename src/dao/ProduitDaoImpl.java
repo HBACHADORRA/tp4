215ff1a27722e482edd813beb928f5a66639ed28
Dorra Hbacha <hbachadorra.info@gmail.com>
2023-03-13 21:51:37 +0100

    first commit

--- src/dao/ProduitDaoImpl.java
+++ src/dao/ProduitDaoImpl.java
@@ -1,106 +1,55 @@
 package dao;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.List;
-import metier.SingletonConnection;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import metier.entities.Produit;
+import util.JPAutil;
+
 public class ProduitDaoImpl implements IProduitDao {
-@Override
-public Produit save(Produit p) {
-Connection conn=SingletonConnection.getConnection();
-try {
+	private EntityManager entityManager=JPAutil.getEntityManager("TP5_JEE");
 
-PreparedStatement ps= conn.prepareStatement("INSERT INTO PRODUITS(NOM_PRODUIT,PRIX) VALUES(?,?)");
+	@Override
+	public Produit save(Produit p) {
+		EntityTransaction tx = entityManager.getTransaction();
+	 	tx.begin();
+	 	entityManager.persist(p);
+	 	tx.commit();
 
-ps.setString(1, p.getNomProduit());
-ps.setDouble(2, p.getPrix());
-ps.executeUpdate();
-PreparedStatement ps2= conn.prepareStatement
-("SELECT MAX(ID_PRODUIT) as MAX_ID FROM PRODUITS");
+		return p;
+	}
 
-ResultSet rs =ps2.executeQuery();
-if (rs.next()) {
-p.setIdProduit(rs.getLong("MAX_ID"));
-}
-ps.close();
-ps2.close();
-} catch (SQLException e) {
-e.printStackTrace();
-}
-return p;
-}
-@Override
-public List<Produit> produitsParMC(String mc) {
-List<Produit> prods= new ArrayList<Produit>();
-Connection conn=SingletonConnection.getConnection();
-try {
+	@Override
+	public List<Produit> produitsParMC(String mc) {
+		List<Produit> prods =
+		         entityManager.createQuery("select p from Produit p where p.nomProduit like :mc")
+		                       .setParameter("mc", "%"+mc+"%")
+		                      .getResultList(); 
+			return prods;
+	}
+	
+	@Override
+	public Produit getProduit(Long id) {
+     	return entityManager.find(Produit.class, id);
+	}
 
-PreparedStatement ps= conn.prepareStatement("select * from PRODUITS where NOM_PRODUIT LIKE ?");
-ps.setString(1, "%"+mc+"%");
-ResultSet rs = ps.executeQuery();
-while (rs.next()) {
-Produit p = new Produit();
-p.setIdProduit(rs.getLong("ID_PRODUIT"));
-p.setNomProduit(rs.getString("NOM_PRODUIT"));
-p.setPrix(rs.getDouble("PRIX"));
-prods.add(p);
-}
-} catch (SQLException e) {
-e.printStackTrace();
-}
-return prods;
+	@Override
+	public Produit updateProduit(Produit p) {
+		EntityTransaction tx = entityManager.getTransaction();
+	 	tx.begin();
+	 	entityManager.merge(p);
+	 	tx.commit();
+		return p;
+	}
 
-}
-@Override
-public Produit getProduit(Long id) {
-Connection conn=SingletonConnection.getConnection();
-Produit p = new Produit();
-try {
+	@Override
+	public void deleteProduit(Long id) {
+		 Produit produit = entityManager.find(Produit.class, id);
 
-PreparedStatement ps= conn.prepareStatement("select * from PRODUITS where ID_PRODUIT = ?");
-ps.setLong(1, id);
-ResultSet rs = ps.executeQuery();
-if (rs.next()) {
-p.setIdProduit(rs.getLong("ID_PRODUIT"));
-p.setNomProduit(rs.getString("NOM_PRODUIT"));
-p.setPrix(rs.getDouble("PRIX"));
-}
-} catch (SQLException e) {
-e.printStackTrace();
-}
-return p;
+		 entityManager.getTransaction().begin();
+		 entityManager.remove(produit);
+		 entityManager.getTransaction().commit();
+		
+	}
 
-}
-@Override
-public Produit updateProduit(Produit p) {
-Connection conn=SingletonConnection.getConnection();
-try {
-
-PreparedStatement ps= conn.prepareStatement("UPDATE PRODUITS SET NOM_PRODUIT=?,PRIX=? WHERE ID_PRODUIT=?");
-ps.setString(1, p.getNomProduit());
-ps.setDouble(2, p.getPrix());
-ps.setLong(3, p.getIdProduit());
-ps.executeUpdate();
-ps.close();
-} catch (SQLException e) {
-e.printStackTrace();
-}
-return p;
-}
-@Override
-public void deleteProduit(Long id) {
-Connection conn=SingletonConnection.getConnection();
-try {
-
-PreparedStatement ps= conn.prepareStatement("DELETE FROM PRODUITS WHERE ID_PRODUIT = ?");
-ps.setLong(1, id);
-ps.executeUpdate();
-ps.close();
-} catch (SQLException e) {
-e.printStackTrace();
-}
-}
 }
